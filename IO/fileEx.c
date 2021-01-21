1061922962ede4ad983a9d14b3372010a679f870
jinleichu <jinleichu@126.com>
2021-01-21 20:52:08 +0800

    processANDthread

--- IO/fileEx.c
+++ IO/fileEx.c
@@ -2,11 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 
+typedef struct stu{
+	int age ;
+	char name[15];
+	float score;
+}STU;
+
 int main(){
 	FILE *fp;
 	struct tm *info;
 	time_t curtime;
 	char time_c[50];
+
+	STU stu[]={{1,"MIke",99},{2,"pony",56.5}};
 	if((fp = fopen("/home/pony/Desktop/cplusplus/IO/stuINFO.txt","w")) == NULL){
 		perror("fopen");
 		return -1;
@@ -20,6 +28,12 @@ int main(){
 		printf("%l\n",ftell(fp));
 		// sprintf(time_c,"%d-
 	}
+	/*
+	if(fwrite(stu,sizeof(STU),2,fp) < 0){
+		perror("fwrite");
+		return -1;
+	}
+	*/
 	fclose(fp);
 	return 0;
 }
